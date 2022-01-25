da9e63d4c8d06fc8456e35045eaa845e17078fc5
Thuy NT <thuthuy.nguyen1509@gmail.com>
2022-01-25 17:24:17 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
     content: [
         "./src/**/*.{html,js,scss}",
+        "./node_modules/tw-elements/dist/js/**/*.js"
     ],
     theme: {
       fontFamily: {
@@ -10,17 +11,15 @@ module.exports = {
         'MyriadProSemiBold': ['Myriad Pro SemiBold', 'sans-serif'], 
       },
       screens:{
+        // 'xxl': {max: '1431px'},
         'xl': {max: '1119px'},
-
         'lg': {max: '1023px'},
-
         'md': {max: '767px'},
-
         'sm': {max: '639px'}
       },
       container:{
         padding: {
-          xl: "0 1rem"
+          xl: "0 1rem",
         }
       },
       backgroundPosition: {
@@ -28,19 +27,11 @@ module.exports = {
       }
     },
     plugins: [
+      require('tw-elements/dist/plugin'),
       function ({ addComponents }) {
         addComponents({
           '.container': {
             maxWidth: '1200px',
-            // '@screen sm': {
-            //   maxWidth: '639px',
-            // },
-            // '@screen md': {
-            //   maxWidth: '767px',
-            // },
-            // '@screen lg': {
-            //   maxWidth: '1023px',
-            // },
             '@screen xl': {
               maxWidth: '1119px',
               padding: '0 10px'
